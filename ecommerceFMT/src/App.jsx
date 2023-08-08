0e84f04ae13f32b1e9c488de4b0d0cb4b3bb428c
briitogabriel <gabriel.brito.a@hotmail.com>
2023-08-08 20:35:40 -0300

    npm install, mock COD e fix pages

--- ecommerceFMT/src/App.jsx
+++ ecommerceFMT/src/App.jsx
@@ -1,15 +1,17 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import ContactPage from '../src/pages/ContactPage/ContactPage';
+import NotFoundPage from '../src/pages/NotFoundPage/NotFoundPage';
 
 function App() {
   return (
     <>
       <Router>
         <Routes>
-         {/*  <Route path="/" element={<HomePage />} />
-          <Route path="/produtos" element={<ProductsPage />} />
-          <Route path="/carrinho" element={<ShoppingCartPage />} />
+          {/* <Route path="/" element={<HomePage />} /> */}
+          {/* <Route path="/produtos" element={<ProductsPage />} /> */}
+          {/* <Route path="/carrinho" element={<ShoppingCartPage />} /> */}
           <Route path="/contato" element={<ContactPage />} />
-          <Route path="*" element={<NotFoundPage />} /> */}
+          <Route path="*" element={<NotFoundPage />} />
         </Routes>
       </Router>
     </>
