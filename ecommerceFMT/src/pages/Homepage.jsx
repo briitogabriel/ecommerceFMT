93d267708064b6e766c77cf4564debe7be062882
Guilherme Ferreira <124100402+guicollab@users.noreply.github.com>
2023-08-08 21:38:51 -0300

    Criando página home (#4)

---
+++ ecommerceFMT/src/pages/Homepage.jsx
@@ -0,0 +1,9 @@
+import { Container } from 'react-bootstrap';
+
+export const Homepage = () => {
+  return (
+    <Container>
+      <h2 className="my-3">Esperando componente produto</h2>
+    </Container>
+  );
+};
