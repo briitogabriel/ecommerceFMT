3b877da252286bb3a0a53101d561115985d33eba
unknown <guilherme.tecnologiaweb@gmail.com>
2023-08-09 16:23:53 -0300

    Ajustando componentes footer/navbar/form

--- ecommerceFMT/src/pages/Homepage/Homepage.jsx
+++ ecommerceFMT/src/pages/Homepage/Homepage.jsx
@@ -3,7 +3,7 @@ import { Container } from 'react-bootstrap';
 export const Homepage = () => {
   return (
     <Container>
-      <h2 className="my-3">Esperando componente produto</h2>
+      <h2 className="my-5 text-center">Aguardando Card Produtos</h2>
     </Container>
   );
 };
