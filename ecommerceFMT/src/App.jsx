342b1ac15e0ce3a79a978d343ded11c428a7139f
unknown <guilherme.tecnologiaweb@gmail.com>
2023-08-08 21:38:04 -0300

    Criando página home

--- ecommerceFMT/src/App.jsx
+++ ecommerceFMT/src/App.jsx
@@ -2,19 +2,20 @@ import { Routes, Route } from 'react-router-dom';
 import ContactPage from '../src/pages/ContactPage/ContactPage';
 import NotFoundPage from '../src/pages/NotFoundPage/NotFoundPage';
 import { Header } from './components/Header';
+import { Homepage } from './pages/Homepage';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <>
       <Header />
-        <Routes>
-          {/* <Route path="/" element={<HomePage />} /> */}
-          {/* <Route path="/produtos" element={<ProductsPage />} /> */}
-          {/* <Route path="/carrinho" element={<ShoppingCartPage />} /> */}
-          <Route path="/contato" element={<ContactPage />} />
-          <Route path="*" element={<NotFoundPage />} />
-        </Routes>
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        {/* <Route path="/produtos" element={<ProductsPage />} /> */}
+        {/* <Route path="/carrinho" element={<ShoppingCartPage />} /> */}
+        <Route path="/contato" element={<ContactPage />} />
+        <Route path="*" element={<NotFoundPage />} />
+      </Routes>
     </>
   );
 }
