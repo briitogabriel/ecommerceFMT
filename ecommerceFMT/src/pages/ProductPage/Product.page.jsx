49fd9318a10f9acf6fccecf12d3a4d8ef6f9150d
Fernanda Gonçalves <fernanda-bess@hotmail.com>
2023-08-09 23:23:41 -0300

    Estilizando página de produto

--- ecommerceFMT/src/pages/ProductPage/Product.page.jsx
+++ ecommerceFMT/src/pages/ProductPage/Product.page.jsx
@@ -12,16 +12,24 @@ export const ProductPage = () => {
     const product = products.filter(product => product.id === productId)
     console.log(product)
     return (
-        <div className='container p-5'>
-            <Card className='m-auto p-3' style={{ width: '60%' }}>
-                <Card.Img src={product[0].imagem} />
-                <Card.Body className='m-auto' >
+        <div className="product p-4">
+            <Card className="m-auto" style={{width: "80%"}}>
+                <div className="row g-0">
+                    <div className="col-md-4 m-auto d-flex justify-content-center">
+                    <img src={product[0].imagem} className="img-fluid rounded-start" alt={product.nome} />
+                    </div>
+                    <div className="col-md-8 d-flex flex-wrap justify-content-evenly">
+                    <Card.Body className='m-auto'>
                     <Card.Title>{product[0].nome}</Card.Title>
-                    <Card.Text className='text-center'>{product[0].descricao}</Card.Text>
-                    <Card.Text>{product[0].preco}</Card.Text>
-                    <Button variant="primary" onClick={() => addProductToCart(product[0].id) }>Adicionar ao carrinho</Button>
-                </Card.Body>
-            </Card>
+                        <Card.Text className='text-center'>{product[0].descricao}</Card.Text>
+                        <Card.Text>{product[0].preco}</Card.Text>
+                        <Button variant="primary" onClick={() => addProductToCart(product[0].id) }>Adicionar ao carrinho</Button>
+                    </Card.Body>
+                    </div>
+                </div>
+                </Card>
         </div>
+
+        
     )
 }
