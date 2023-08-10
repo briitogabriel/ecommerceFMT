9dac1f80e883b5015cb17a9910418dbf5a58b92f
Fernanda Gonçalves <fernanda-bess@hotmail.com>
2023-08-10 20:36:00 -0300

    style not found page

--- ecommerceFMT/src/pages/NotFoundPage/NotFoundPage.jsx
+++ ecommerceFMT/src/pages/NotFoundPage/NotFoundPage.jsx
@@ -1,6 +1,9 @@
+import {TbFaceIdError} from 'react-icons/tb'
+
 export const NotFoundPage = () => {
   return (
-    <div className="d-flex flex-column align-items-center">
+    <div className="d-flex flex-column align-items-center justify-content-center" style={{height: '45vh'}}>
+      <div className='fs-1'><TbFaceIdError/></div>
       <h1 className="text-secondary mt-5">404 Página não encontrada!</h1>
       <img src="/404.jpg" alt="" width={600} />
     </div>
