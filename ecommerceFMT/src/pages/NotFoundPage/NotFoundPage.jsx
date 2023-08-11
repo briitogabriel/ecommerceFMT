b6c5e1d0fdb5e77427c30b278f54c869d51f50e1
briitogabriel <gabriel.brito.a@hotmail.com>
2023-08-10 21:43:15 -0300

    remover img não existente

--- ecommerceFMT/src/pages/NotFoundPage/NotFoundPage.jsx
+++ ecommerceFMT/src/pages/NotFoundPage/NotFoundPage.jsx
@@ -5,7 +5,6 @@ export const NotFoundPage = () => {
     <div className="d-flex flex-column align-items-center justify-content-center" style={{height: '45vh'}}>
       <div className='fs-1'><TbFaceIdError/></div>
       <h1 className="text-secondary mt-5">404 Página não encontrada!</h1>
-      <img src="/404.jpg" alt="" width={600} />
     </div>
   );
 };
