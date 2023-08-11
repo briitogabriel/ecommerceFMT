f9d5328d65adae19badffd2387ab6d6884f9934e
Guilherme Ferreira <124100402+guicollab@users.noreply.github.com>
2023-08-11 00:07:54 -0300

    Ajustando página checkout & adicionando função para formatar moeda (#27)

--- ecommerceFMT/src/context/Context.jsx
+++ ecommerceFMT/src/context/Context.jsx
@@ -1,44 +1,58 @@
 import { createContext, useContext, useEffect, useState } from 'react';
-import data from '../assets/mock.json'
+import data from '../assets/mock.json';
 
 export const AppContext = createContext();
 
 export const AppContextProvider = ({ children }) => {
-  const [products, setProducts] = useState(data)
-  const [cart, setCart] = useState([])
-  const [total, setTotal] = useState(0)
+  const [products, setProducts] = useState(data);
+  const [cart, setCart] = useState([]);
+  const [total, setTotal] = useState(0);
 
   const totalCart = () => {
     const total = cart.reduce((prev, item) => {
-      return prev + item.preco * item.quantidade
-    }, 0)
-    setTotal(total)
-  }
+      return prev + item.preco * item.quantidade;
+    }, 0);
+    setTotal(total);
+  };
 
-  const addProductToCart = (id)  => {
-    const checkItem = cart.every(item => item.id !== id)
+  const addProductToCart = (id) => {
+    const checkItem = cart.every((item) => item.id !== id);
     if (checkItem) {
-      const item = products.filter(product => product.id === id)
-      item[0].quantidade += 1
-      const novoItem = {...item[0]}
-      setCart([...cart, novoItem ])
+      const item = products.filter((product) => product.id === id);
+      item[0].quantidade += 1;
+      const novoItem = { ...item[0] };
+      setCart([...cart, novoItem]);
     } else {
-      setCart(cart.map(item => item.id === id ? { ...item, quantidade: item.quantidade + 1 } : item))
+      setCart(
+        cart.map((item) =>
+          item.id === id ? { ...item, quantidade: item.quantidade + 1 } : item
+        )
+      );
     }
-  }
+  };
 
   useEffect(() => {
-    totalCart()
-  }, [cart])
-  
+    totalCart();
+  }, [cart]);
 
   return (
-    <AppContext.Provider value={{products, setProducts, addProductToCart, cart, setCart, totalCart, total}}>
+    <AppContext.Provider
+      value={{
+        products,
+        setProducts,
+        addProductToCart,
+        cart,
+        setCart,
+        totalCart,
+        total,
+        setTotal,
+      }}
+    >
       {children}
     </AppContext.Provider>
-  )
-}
+  );
+};
 
 export const useAppContext = () => {
-  return useContext(AppContext)
-}
+  return useContext(AppContext);
+};
--- ecommerceFMT/src/pages/ProductPage/Product.page.jsx
+++ ecommerceFMT/src/pages/ProductPage/Product.page.jsx
@@ -1,44 +1,56 @@
-import {useParams} from 'react-router-dom';
+import { useParams } from 'react-router-dom';
 import { useAppContext } from '../../context/Context';
-import {Button, Card} from 'react-bootstrap';
+import { Button, Card } from 'react-bootstrap';
 import { LinkContainer } from 'react-router-bootstrap';
 import './ProductPage.style.css';
-import {BiArrowBack} from 'react-icons/bi'
+import { BiArrowBack } from 'react-icons/bi';
+import { formatCurrency } from './../../utils/formatCurrency';
 
 export const ProductPage = () => {
-    const {id} = useParams();
-    const { addProductToCart, products } = useAppContext();
+  const { id } = useParams();
+  const { addProductToCart, products } = useAppContext();
 
-    const productId = Number(id);
+  const productId = Number(id);
 
-    const product = products.filter(product => product.id === productId)
-    
-    const path = `http://${window.location.host}`
+  const product = products.filter((product) => product.id === productId);
 
-    return (
-        <div className="product p-4 text-center" style={{height: "45vh"}}>
-            <Card className="m-auto" style={{width: "80%"}}>
-                <div className="row g-0">
-                    <div className="col-md-4 m-auto d-flex justify-content-center">
-                    <img src={`${path}/${product[0].imagem}`} className="img-fluid rounded-start" alt={product.nome} />
-                    </div>
-                    <div className="col-md-8 d-flex flex-wrap justify-content-evenly">
-                    <Card.Body className='m-auto'>
-                    <Card.Title>{product[0].nome}</Card.Title>
-                        <Card.Text className='text-center'>{product[0].descricao}</Card.Text>
-                        <Card.Text>{product[0].preco}</Card.Text>
-                        <Button variant="primary" onClick={() => addProductToCart(product[0].id) }>Adicionar ao carrinho</Button>
-                    </Card.Body>
-                    </div>
-                </div>
-                </Card>
-                <LinkContainer to="/">
-                <div className='mt-1'>
-                    <a className='return-home'><BiArrowBack/> Voltar para página inicial</a>
-                </div>
-                </LinkContainer>
+  const path = `http://${window.location.host}`;
+
+  return (
+    <div className="product p-4 text-center" style={{ height: '45vh' }}>
+      <Card className="m-auto" style={{ width: '80%' }}>
+        <div className="row g-0">
+          <div className="col-md-4 m-auto d-flex justify-content-center">
+            <img
+              src={`${path}/${product[0].imagem}`}
+              className="img-fluid rounded-start"
+              alt={product.nome}
+            />
+          </div>
+          <div className="col-md-8 d-flex flex-wrap justify-content-evenly">
+            <Card.Body className="m-auto">
+              <Card.Title>{product[0].nome}</Card.Title>
+              <Card.Text className="text-center">
+                {product[0].descricao}
+              </Card.Text>
+              <Card.Text>{formatCurrency(product[0].preco)}</Card.Text>
+              <Button
+                variant="primary"
+                onClick={() => addProductToCart(product[0].id)}
+              >
+                Adicionar ao carrinho
+              </Button>
+            </Card.Body>
+          </div>
         </div>
-
-        
-    )
-}
+      </Card>
+      <LinkContainer to="/">
+        <div className="mt-1">
+          <a className="return-home">
+            <BiArrowBack /> Voltar para página inicial
+          </a>
+        </div>
+      </LinkContainer>
+    </div>
+  );
+};
